5989cedb3a3f39f3afad28d1d9d0e2f4364eaa46
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-04-30 00:21:37 +0530

    add modules with features of movie & movie_details

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+//buildscript{
+//    ext{
+//        compose_ui_version = "1.4.0-alpha"
+//    }
+//}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "MultiModuleProjectBasics"
 include(":app")
- 
+include(":feature:movie:data")
+include(":feature:movie:domain")
+include(":feature:movie:ui")
+include(":feature:movie_details:data")
+include(":feature:movie_details:domain")
+include(":feature:movie_details:ui")
